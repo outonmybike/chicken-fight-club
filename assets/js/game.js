abd93706779153fbf78bac3504900eaeb7444ce1
DAN NELSON <dannelson@DANs-Air.lan>
2020-11-20 16:06:31 -0700

    js intro stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert('This is an alert. JavaScript is running');
+function fight() {
+	window.alert('The fight has begun');
+}
+
+var playerName = window.prompt('What is your robot\'s name?');
+
+console.log(playerName);
+
+// fight();
+
+console.log('This logs a string, good for leaving yourself a note');
+console.log(10+10)
+console.log('The robots name is '+ playerName);
