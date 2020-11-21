844e8daa3c574fc2bdaeaa62f58e7ac1ba8bb654
DAN NELSON <dannelson@DANs-Air.lan>
2020-11-20 18:31:41 -0700

    debugged loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,8 @@
+window.alert('Welcome to Chicken Fight Club');	
 var chickenName = window.prompt('Please state your chicken\'s given name:');
 // var chickenName = 'Fred'
 var chickenHealth = 1000;
-var chickenAttack = 100;
+var chickenAttack = 25;
 var chickenMoney = 10;
 
 var enemyNameList = ['Fog Horn Leg Horn','Chicken Little','Colonel Sanders']
@@ -9,55 +10,57 @@ var enemyHealth = '50';
 var enemyAttack = '12';
 
 var fight = function(enemyName) {
-	window.alert('Welcome to Chicken Fight Club');
+	while(enemyHealth>0) {
 
-	var promptFight = window.prompt('Would you like to FIGHT or SKIP this round?');
+		var promptFight = window.prompt('Would you like to FIGHT or SKIP this round?');
 
-	if (promptFight.toLowerCase() === 'fight') {
-		//subtract enemy health 
-		enemyHealth = enemyHealth - chickenAttack;
+		if (promptFight.toLowerCase() === 'fight') {
+			//subtract enemy health 
+			enemyHealth = enemyHealth - chickenAttack;
 
-		//log result
-		console.log(chickenName+' attacked '+ enemyName+'. '+enemyName+' now has '+ enemyHealth+' health remaining');
-		//subtract player health
-		chickenHealth = chickenHealth - enemyAttack;
-		//log result
-		console.log(enemyName+' has attacked '+chickenName+'. '+chickenName+' now has '+chickenHealth+' health remaining');
+			//log result
+			console.log(chickenName+' attacked '+ enemyName+'. '+enemyName+' now has '+ enemyHealth+' health remaining');
+			//subtract player health
+			chickenHealth = chickenHealth - enemyAttack;
+			//log result
+			console.log(enemyName+' has attacked '+chickenName+'. '+chickenName+' now has '+chickenHealth+' health remaining');
 
-		//enemy health check
-		if (enemyHealth<=0) {
-			window.alert(enemyName+' has flown to the big coop in the sky');
-		}
-		else {
-			window.alert(enemyName+' has '+enemyHealth+' health remaining');
-		}
+			//enemy health check
+			if (enemyHealth<=0) {
+				window.alert(enemyName+' has flown to the big coop in the sky');
+			}
+			else {
+				window.alert(enemyName+' has '+enemyHealth+' health remaining');
+			}
 
-		//player health check
-		if(chickenHealth <=0) {
-			window.alert(chickenName+' has flown to the big coop in the sky');
+			//player health check
+			if(chickenHealth <=0) {
+				window.alert(chickenName+' has flown to the big coop in the sky');
+			}
+			else {
+				window.alert(chickenName+' has '+chickenHealth+' health remaining')
+			}
+		} else if (promptFight.toLowerCase() === 'skip') {
+			//confirm skip
+			var confirmSkip = window.confirm('You sure you want to quit?');
+			//if confirmed to leave
+			if(confirmSkip) {
+				window.alert(chickenName+' has chosen to skip the fight. (You literally just "Chickened Out")');
+				chickenMoney = chickenMoney - 2;
+			}
+			else {
+				fight();
+			}		
+		} else {
+			window.alert('You had one job. Type the word "Fight" or "Skip". Try again');
 		}
-		else {
-			window.alert(chickenName+' has '+chickenHealth+' health remaining')
-		}
-	} else if (promptFight.toLowerCase() === 'skip') {
-		//confirm skip
-		var confirmSkip = window.confirm('You sure you want to quit?');
-		//if confirmed to leave
-		if(confirmSkip) {
-			window.alert(chickenName+' has chosen to skip the fight. (You literally just "Chickened Out")');
-			chickenMoney = chickenMoney - 2;
-		}
-		else {
-			fight();
-		}		
-	} else {
-		window.alert('You had one job. Type the word "Fight" or "Skip". Try again');
 	}
-
 };
 
 for(var i = 0; i < enemyNameList.length; i++) {
-	fight(enemyNameList[i]);
+	var pickedEnemyName = enemyNameList[i];
+	enemyHealth = 50;
+	fight(pickedEnemyName);
 }
 
 
